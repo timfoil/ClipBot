576db8dadad4987717a6c735a1831b4cc7a5dc5c
unknown <tim@travitz.net>
2018-07-15 09:32:09 -0400

    Create generateSpecificHelpMsg

--- src/sound/SoundContext.js
+++ src/sound/SoundContext.js
@@ -13,21 +13,23 @@ class SoundContext {
      * Produces a help string to send the user, returns cmd specific help if cmd matches a cmd
      */
     getHelpString(cmd) {
-        
         if(cmd) {
-            return generateGenericHelpStr(this.sounds, prefix);
+            return generateGenericHelpStr(this.sounds, this.prefix);
         } else {
-            return generateSpecificHelpMsg();// TODO
+            return generateSpecificHelpMsg(this.sounds, this.prefix); // TODO
         }
-        
     }
 }
 
 function generateGenericHelpStr(sounds, prefix) {
-    const cmds = Array.from(this.sounds, x => this.prefix + x);
-    const helpCommand = this.prefix + 'help';
+    const cmds = Array.from(sounds, x => prefix + x);
+    const helpCommand = prefix + 'help';
 
     return beginHelp + cmds.toString() + endHelp + helpCommand;
+}
+
+function generateSpecificHelpMsg(sounds, prefix, cmd) {
+    return null; // TODO
 }
 
 const beginHelp = 'Play a sound from airhornbot by sending a command in the discord chat! The following is a list of commands:\n';
