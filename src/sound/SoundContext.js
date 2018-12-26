6993adbd7a7f62a38644e1a7e65f01d2d88f7518
unknown <tim@travitz.net>
2018-12-26 12:57:41 -0500

    Create method to get a sound from soundcontext based on a given command

--- src/sound/SoundContext.js
+++ src/sound/SoundContext.js
@@ -33,6 +33,34 @@ class SoundContext {
         }, this);
     }
 
+    /**
+     * Given a (prefixless) command return a soundpath to a requested soundfile
+     * @param {string} cmd issued by the user
+     * @returns a path to a sound file desired by the command or null if no sound exists
+     */
+    getSoundFromGroup(cmd) {
+        // remove trailing spaces
+        const spacelessCmd = cmd.trim();
+        const paramSep = spacelessCmd.indexOf(' ');
+        if (paramSep === -1) {
+            if (this.hasSoundGroup(cmd)) {
+                return this.soundGroups[cmd].getRandomSound(); // get a rando sound
+            }
+        }
+
+        //get sound group/collection
+        const command = cmd.slice(0, paramSep);
+
+        //get sound specifier (not including the space separator)
+        const param = cmd.slice(paramSep + 1, cmd.length);
+
+        if (this.hasSoundGroup(command) && this.soundGroups[command].hasSpecificSound(param)) {
+            return this.soundGroups[command].getSpecificSound(param);
+        }
+
+        // Could not find command, return null
+        return null;
+    }
 
     hasSoundGroup(group) {
         return group in this.soundGroups;
