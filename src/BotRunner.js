1a33cafdbc11e7c631a238d97b638d1f8e269161
timfoil <timpks@me.com>
2019-04-13 17:50:58 -0400

    Print error to console if an unexpected one occurs while running

--- src/BotRunner.js
+++ src/BotRunner.js
@@ -3,9 +3,13 @@ const SoundBot = require('./bot/SoundBot');
 // use destructuring to access config info in our config-file, fancy!
 const {token, prefix, soundDir} = require('./config.json');
 
+try {
+    console.log('Creating bot...');
+    const bot = new SoundBot(token, soundDir, prefix);
 
-console.log('Creating bot...');
-const bot = new SoundBot(token, soundDir, prefix);
-
-console.log('Starting bot...');
-bot.start();
+    console.log('Starting bot...');
+    bot.start();
+} catch (e) {
+    console.log('Unexpected error occured while running bot');
+    console.log(e);
+}
